3f922ae3b1c993bf6601c24ef070d6270541ba72
Angela Taylor <djdessert@gmail.com>
2024-03-13 15:40:23 +0700

    added md lg xl and 2xl responive styling

--- app/page.js
+++ app/page.js
@@ -20,20 +20,21 @@ export default function Home() {
 
           {/* Nav Menu */}
 
-          <nav>
-            <div className="text-white text-2xl font-bold flex flex-col text-center py-10">
+          <nav className="flex flex-col md:flex-row lg:w-9/12 lg:mx-auto lg:pt-12">
+            <div className="text-white text-2xl font-bold flex flex-col text-center py-10 md:pr-10 lg:pr-0">
               CollierTechnologies©</div>
 
-            <div>
-              <ul className="flex justify-evenly text-white py-10 lg:ml-20">
+            <div className="xl:ml-auto">
+              <ul className="flex justify-evenly text-white py-10  md:gap-10 md:mx-10 md:whitespace-nowrap lg:gap-8 
+              lg:mr-0 lg:ml-20 xl:mr-16 ">
                 <li>
                   <div className="flex flex-col">
-                    <a className="cursor-pointer text-l lg:px-3 text-lime-500" href="#">Home</a>
+                    <a className="cursor-pointer text-l  text-lime-500" href="#">Home</a>
                     <Image className="mx-0.5" src={underline} />
                   </div></li>
                 <li><a className="cursor-pointer text-l lg:px-3" href="#">Services</a></li>
                 <li><a className="cursor-pointer text-l lg:px-3" href="#">How It Works</a></li>
-                <li><a className="cursor-pointer text-l lg:px-3 lg:pr-64" href="#">Contact</a></li>
+                <li><a className="cursor-pointer text-l lg:px-3 " href="#">Contact</a></li>
               </ul>
 
             </div>
@@ -42,23 +43,28 @@ export default function Home() {
         {/* Main Site Image & Title */}
 
         <div className="flex flex-row bg-[url('.//public/charcuteriesnack70.JPG')] 
-          w-auto h-72 bg-cover">
+          w-auto h-72 bg-cover md:w-3/4 md:mx-auto lg:h-auto">
 
-            <div className="flex flex-col">
-              <h2 className="text-lime-500 font-semibold mx-8 my-4 text-2xl">WELCOME TO</h2>
-              <h1 className="text-white text-5xl mx-8 font-semibold"> Your Small Business</h1>
-              <p className="text-white m-8 font-semibold">Custom Tailored Website</p>
+            <div className="flex flex-col ">
+              <h2 className="text-lime-500 font-semibold mx-8 my-4 text-2xl lg:my-8 lg:text-3xl">WELCOME TO</h2>
+              <h1 className="text-white text-5xl mx-8 font-semibold lg:text-6xl"> Your Small Business</h1>
+              <p className="text-white m-8 font-semibold lg:text-2xl">Custom Tailored Website</p>
             </div>
 
-          <div className= "self-center mx-4">
+          <div className= "self-center mx-4 lg:w-48 xl:ml-auto ">
             <Image className="h-64 w-36" src={katakana} />
           </div>
 
         </div>
 
-        <div className="flex flex-col m-8">
+
+        {/* Customized */}
+
+
+        <div className="flex flex-col mx-8 -mb-10 sm:mb-10 sm:mt-16 md:mt-20 md:-mb-20 lg:-mb-36 lg:mt-48 xl:-mb-48">
           <div className="text-lime-400 font-semibold  text-3xl text-center mt-24">CUSTOMIZED</div>
-          <div className="text-white font-semibold  text-4xl text-center mt-10">
+          <div className="text-white font-semibold  sm:mt-4 sm:w-5/6 sm:mx-auto text-4xl text-center mt-10 md:w-3/6 
+          md:-mb-8 lg:w-2/6 2xl:-mb-16 2xl:text-5xl">
             Web Development Services</div>
         </div>
 
@@ -68,7 +74,7 @@ export default function Home() {
       {/* First White Triangles */}
 
 
-        <div className="flex flex-row">
+        <div className="flex flex-row sm:-mt-20 md:mt-2">
           <Image className="resize w-3/6" src={LeftV} />
           <Image className="resize w-3/6" src={RightV} />
         </div>
@@ -77,11 +83,12 @@ export default function Home() {
       {/* Experience Section */}
 
 
-        <div className="flex flex-col m-8">
+        <div className="flex flex-col mx-8 -mt-6 sm:-mt-12 md:-mt-24  lg:-mt-40 lg:mb-36 xl:-mt-48 2xl:-mt-64">
           <div className="text-lime-400 font-semibold  text-3xl text-center">EXPERIENCE</div>
-          <div className="text-white font-semibold  text-4xl text-center mt-10">
-            Serving for More Than 15 Years</div>
-          <div className="text-white text-center text-sm font-light leading-6 my-6">
+          <div className="text-white font-semibold  text-4xl text-center mt-10 sm:w-4/6 sm:mx-auto md:w-5/12 
+          md:mx-auto md:mb-10 2xl:text-5xl">Serving for More Than 15 Years</div>
+          <div className="text-white text-center text-sm font-light leading-6 my-6 sm:mb-12 md:mt-auto lg:w-9/12 lg:mx-auto 
+          lg:text-base lg:leading-7 xl:leading-8 xl:w-7/12 xl:mt-10 2xl:mt-2">
             <p>Welcome to CollierTech, where 15 years of expertise converge to offer unmatched 
               web development and hosting solutions. From bespoke websites tailored to your needs 
               to reliable hosting services ensuring 24/7 accessibility, we're dedicated to empowering 
@@ -96,19 +103,20 @@ export default function Home() {
       {/* 1st White Divider Div */}
 
 
-      <div className="h-44 bg-white"></div>
+      <div className="h-44 md:h-96 bg-white"></div>
 
 
     {/* Current Offering Div */}
 
 
-      <div className="flex flex-col">
-        <h2 className="text-white font-semibold mt-20 text-5xl text-center">Current Offering</h2>
-        <Image className="p-10" src= {croissant} />
-        <h1 className="text-white px-10 text-3xl font-bold">Small Business Site</h1>
+      <div className="flex flex-col w-5/6 m-auto mb-12 md:w-2/4 md:mx-auto lg:my-20 lg:w-2/5 ">
+        <h2 className="text-white font-semibold mt-20 text-5xl text-center lg:text-5xl 2xl:text-6xl">Current Offering</h2>
+        <Image className="p-10 md:h-80 md:w-96 md:m-auto lg:h-96 lg:w-auto" src= {croissant} />
+        <h1 className="text-white px-10 md:px-8 lg:px-10 2xl:pl-28 text-3xl font-bold">Small Business Site</h1>
 
-        <div className="flex mb-10">
-          <div className="text-white text-base p-10 flex-1">1-3 pages, 14 days delivery</div>
+        <div className="flex mb-10 2xl:mb-20">
+          <div className="text-white text-base p-10 md:px-8 lg:px-10 2xl:pl-28 2xl:pr-0 
+          whitespace-nowrap  flex-1">1-3 pages, 14 days delivery</div>
           <div className="text-white text-3xl font-bold my-auto text-center flex-1">$350</div>
         </div>
       </div>
@@ -117,22 +125,23 @@ export default function Home() {
     {/* 2nd White Divider Div */}
 
 
-      <div className="h-44 bg-white"></div>
+      <div className="h-44 md:h-96 bg-white"></div>
 
 
     {/* How It Works Div */}
 
 
     <div className="mb-10">
-      <h1 className="text-white text-4xl mx-8 font-bold text-center p-10"> How It Works</h1>
+      <h1 className="text-white text-4xl mx-8 mt-12 font-bold text-center p-10 lg:my-12 lg:text-5xl 2xl:mt-20"> How It Works</h1>
 
       <div className="flex flex-row bg-[url('.//public/ramengray40.jpg')] 
-      w-auto h-72 bg-cover mx-10 ">
+      w-4/6 mx-auto rounded-md h-72 bg-cover mx-10 md:w-2/4 md:mx-auto lg:w-2/6 md:rounded-lg lg:rounded-2xl lg:mb-24">
 
-        <div className="flex flex-col">
-          <h1 className="text-white text-3xl mx-8 mt-8 text-center font-semibold"> 
+        <div className="flex flex-col ">
+          <h1 className="text-white text-3xl mx-8 mt-8 text-center font-semibold whitespace-nowrap"> 
             1. Fill Out Site Survey</h1>
-          <p className="text-white m-8 text-sm leading-6 text-justify">Start by filling out a quick 
+          <p className="text-white m-8 text-sm lg:text-base leading-6 text-justify lg:mx-6 lg:font-semibold 
+          2xl:w-5/6 2xl:mx-auto">Start by filling out a quick 
             survey tailored to understand your small business website needs. Your responses will guide 
             our developers in comprehending your requirements better, ensuring we deliver exactly 
             what you want for your business's digital presence.</p>
@@ -143,15 +152,16 @@ export default function Home() {
       <div className="m-10"></div>
 
       <div className="flex flex-row bg-[url('.//public/shrimptacosgray40.jpg')] 
-        w-auto h-72 bg-cover mx-10 ">
+        w-4/6 mx-auto rounded-md h-72 bg-cover mx-10 md:w-2/4 md:mx-auto lg:w-2/6 lg:rounded-2xl lg:mb-24">
 
           <div className="flex flex-col">
             <h1 className="text-white text-3xl mx-8 mt-8 text-center font-semibold"> 
-              2. Meet & Discuss </h1>
-            <p className="text-white m-8 text-sm leading-6 text-justify">We’ll transform your survey 
-              input into a mock website template, setting the stage for your digital journey. Next, we 
-              schedule a 1:1 meeting to refine details before the build. Your feedback ensures we align 
-              perfectly with your vision, crafting a website that exceeds expectations.</p>
+              2. Prototype Build</h1>
+            <p className="text-white m-8 text-sm lg:text-base leading-6 text-justify lg:mx-6 lg:font-semibold 
+            2xl:w-5/6 2xl:mx-auto ">Next we’ll use your 
+            survey input to build your tailor-made website for you, and share it with you within 
+            1 week. At that time you can review and submit any edits or revisions, which we’ll 
+            incorporate.</p>
           </div>
 
       </div>
@@ -159,23 +169,27 @@ export default function Home() {
       <div className="m-10"></div>
 
       <div className="flex flex-row bg-[url('.//public/skilletbreakfastgray40.jpg')] 
-        w-auto h-72 bg-cover mx-10 ">
+        w-4/6 mx-auto rounded-md h-72 bg-cover mx-8 md:w-2/4 md:mx-auto lg:w-2/6 lg:rounded-2xl lg:mb-24">
 
           <div className="flex flex-col">
-            <h1 className="text-white text-3xl mx-8 mt-8 text-center font-semibold"> 
-              3. Build, Refine, Deliver</h1>
-            <p className="text-white m-8 text-sm leading-6 text-justify">Our developers start building 
-            your website after our discussions. In just one week, we'll send a prototype for your review. 
-            We're open to one round of revisions. After two weeks, your fully realized website is ready 
-            for launch.</p>
+            <h1 className="text-white text-3xl mx-8 lg:mx-0 mt-8 text-center font-semibold"> 
+              3. Refine, Publish, Deliver</h1>
+            <p className="text-white m-8 text-sm lg:text-base leading-6 text-justify lg:mx-6 lg:font-semibold 
+            2xl:w-5/6 2xl:mx-auto">We’ll spend 1 week 
+            making sure we’ve captured all of your revisions, then finish and publish the site 
+            online. Finally we’ll hand off everything to you, with the site ready to go.</p>
           </div>
 
       </div>
     </div>
-      
-    <div className="flex flex-col m-8 mb-0">
+
+
+    {/* About Us */}
+
+
+    <div className="flex flex-col m-8 mb-0 md:w-3/6 md:mx-auto md:-mb-24 md:mt-40 md:w-4/6 lg:-mb-72 lg:mt-48 lg:w-3/6">
       <div className="text-lime-400 font-semibold  text-3xl text-center mt-24">ABOUT US</div>
-      <div className="text-white font-semibold  text-4xl text-center mt-10">
+      <div className="text-white font-semibold  text-4xl text-center mt-10 w-4/6 mx-auto -mb-8 2xl:text-5xl 2xl:mb-0">
         We Build Beautiful Websites for You</div>
     </div>
 
@@ -184,25 +198,29 @@ export default function Home() {
 
 
     <div className="flex">
-      <Image className="resize w-6/6 flex-1 h-96" src={LowerV} />
+      <Image className=" flex-1 h-96 md:h-svh md:-mt-12 lg:mt-36" src={LowerV} />
     </div>
 
 
     {/* About Us Section - Background Experience */}
 
-    <div className="flex flex-col text-white text-sm leading-7 text-justify p-10 mt-6">
-      <p>CollierTech brings over a decade of expertise in software development, systems administration, 
-        and network operations, finely tuned for small businesses. With a proven track record of 
-        completing numerous projects in various programming languages and deployment automation tools, 
-        we ensure efficient processes tailored to your needs. </p>
-      <p>Our satisfied clientele includes LiquidWeb, Doubleprime, Cadmium Yellow Design, and Computational 
-        Biology Research Center. Each project showcases our dedication to delivering top-notch solutions 
-        that align perfectly with our clients' goals. Trust CollierTech to bring precision, innovation, 
-        and customer-centricity to your digital endeavors.</p>
+    <div className="flex flex-col text-white text-sm leading-7 text-justify p-10 mx-auto  mt-6 md:mt-0 xl:p-20 2xl:leading-8 2xl:text-lg">
+      <div className="flex flex-col md:flex-row">
+        <div className="md:m-4">CollierTech brings over a decade of expertise in software development, systems administration, 
+          and network operations, finely tuned for small businesses. With a proven track record of 
+          completing numerous projects in various programming languages and deployment automation tools, 
+          we ensure efficient processes tailored to your needs. </div>
+        <div className="md:m-4">Our satisfied clientele includes LiquidWeb, Doubleprime, Cadmium Yellow Design, and Computational 
+          Biology Research Center. Each project showcases our dedication to delivering top-notch solutions 
+          that align perfectly with our clients' goals. Trust CollierTech to bring precision, innovation, 
+          and customer-centricity to your digital endeavors.</div>
+      </div>
 
-      <div className="text-center p-4">
-        <a className="text-white cursor-pointer text-lg font-semibold" href="#">View 
-        Resume here for more details</a>
+
+      <div className="text-center p-4 mt-4 md:mb-16 2xl:mt-12">
+        <a className="text-lime-400 cursor-pointer text-lg font-semibold 2xl:text-2xl" 
+        href="https://colliertech.org/~cjac/resume/cjac.html">
+          View Resume here for more details</a>
       </div>
 
     </div>
@@ -211,15 +229,21 @@ export default function Home() {
     {/* Client Form Section */}
 
 
-    <div className="flex flex-row bg-[url('.//public/FormLinkBkgnd.svg')] w-auto h-96 bg-cover">
+    <div className="flex flex-row bg-[url('.//public/FormLinkBkgnd.svg')] w-auto h-96 bg-cover md:w-3/4 
+    md:rounded-lg lg:rounded-2xl md:mx-auto lg:w-4/6">
 
-      <div className="flex flex-col p-10">
-        <h2 className="text-white font-semibold mx-8 my-4 text-center text-3xl">Fill Out client
-          Website Form Here</h2>
-        <p className="text-white m-8 leading-8 text-base text-justify ">Click here to access our Website Request 
-        form, giving us details on what you’d like to see on your site and how you’d like 
-        it to behave.</p>
-        <button className="bg-lime-400 rounded-2xl w-28 p-2 mx-auto whitespace-nowrap">Access Form</button>
+      <div className="flex flex-col p-8 md:m-auto lg:w-auto lg:flex-row lg:p-2 2xl:w-5/6">
+        <div>
+          <h2 className="text-white font-semibold mx-8 my-4 md:text-center text-3xl lg:mx-16">Fill Out client
+            Website Form Here</h2>
+          <p className="text-white m-8 md:my-12 leading-8 text-base text-justify lg:px-12 lg:text-lg ">Click here to access our Website Request 
+          form, giving us details on what you’d like to see on your site and how you’d like 
+          it to behave.</p>
+        </div>
+       <div className="m-auto lg:px-16">
+        <button className="bg-lime-400 rounded-2xl w-28 p-3 mx-auto whitespace-nowrap">Access Form</button>
+       </div>
+
       </div>
 
 
@@ -229,10 +253,10 @@ export default function Home() {
     {/* Footer Section */}
 
 
-    <div className="text-white  flex flex-col text-center py-10">
-      <div className="font-bold text-2xl mt-16 mb-6">CollierTechnologies©</div>
-      <div className="text-xl mb-6">cjac@colliertech.com</div>
-      <div className="text-xl">+1 425 523 2637</div>
+    <div className="text-white  flex flex-col text-center py-10 md:mt-16 md:flex-row md:justify-center lg:pt-20">
+      <div className="font-bold text-2xl mt-16 mb-6 md:mr-16 lg:my-auto lg:py-10">CollierTechnologies©</div>
+      <div className="text-xl mb-6 md:mt-16 md:mr-16 lg:my-auto lg:py-10">cjac@colliertech.com</div>
+      <div className="text-xl md:mt-16 lg:my-auto lg:py-10">+1 425 523 2637</div>
     </div>
 
       </section>
